a403c89239354c76c97053ae7676a8da4b8db8e1
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-10-12 11:30:47 +0330

    article fragment kotlin file creatd

---
+++ app/src/main/java/com/example/newsapplication/ui/ArticleFragment.kt
@@ -0,0 +1,25 @@
+package com.example.newsapplication.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.newsapplication.R
+import com.example.newsapplication.databinding.FragmentArticleBinding
+
+class ArticleFragment : Fragment() {
+    private lateinit var binding: FragmentArticleBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        this.binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_article, container, false)
+        this.binding.lifecycleOwner = this
+        return this.binding.root
+    }
+}
