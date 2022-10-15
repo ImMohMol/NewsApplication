0ef15d8ae2ba6a263a4a32ee2143cc12c65741b7
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-10-15 23:53:34 +0330

    retrofit jobs is done :)

---
+++ app/src/main/java/com/example/newsapplication/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapplication.ui.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "d77cd2afe53e4e2e8bcd80a66b5407a8"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
