46def0655c3b37877c22e563c8ae8b8ff39ef394
Mij_Mhm8013 <mhoseinmola1380@gmail.com>
2022-10-15 23:31:18 +0330

    the navigation added

--- app/src/main/java/com/example/newsapplication/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapplication/ui/NewsActivity.kt
@@ -1,12 +1,35 @@
 package com.example.newsapplication.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsapplication.R
+import com.example.newsapplication.databinding.ActivityNewsBinding
 
 class NewsActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityNewsBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        this.binding = ActivityNewsBinding.inflate(this.layoutInflater)
+        setContentView(this.binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.newsNavHostFragment
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        this.binding.bottomNavigationView.setupWithNavController(navController)
+
+        this.appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.breakingNewsFragment, R.id.savedNewsFragment, R.id.searchNewsFragment)
+        )
+        this.setupActionBarWithNavController(navController, this.appBarConfiguration)
     }
 }
